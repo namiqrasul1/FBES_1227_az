b7c90c9836174177ab9e9fb762645622c7b4c831
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2023-01-18 21:52:03 +0400

    Lesson 10. Generic, Generic Collection, Iterator

---
+++ C#/Lesson10/Lesson10/Class1.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+
+namespace Lesson10
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // non-generic 
+            //ArrayList al = new();
+            //al.Add(1);
+            //al.Add("rustam");
+            //al.Add(new Program());
+            //int? i = al[0] as int?;
+            //// ? => nullable
+            //// ? => null conditional
+
+            //ArrayList al = new();
+            //al.Add(1);
+            //al.Add(2);
+            //al.Add(3);
+            //al.Add("hakuna");
+            //al.Add(5);
+            //al.Add(7);
+
+            //foreach (object item in al)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //var enumerator = al.GetEnumerator();
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current);
+            //}
+            //enumerator.Reset();
+            //enumerator.MoveNext();
+            //Console.WriteLine(enumerator.Current);
+
+            // boxing   => value type -> reference type  
+            // unboxing => reference type -> value type
+
+            // generic
+            //List<int> l = new() { 1, 23, 123, 12, 312, 4523, 5 };
+            //List<int> list = new();
+            //list.Add(1);   
+            //list.Add(2);   
+            //list.Add(4);   
+            //list.Add(56);   
+            //list.Add(7);
+            //list.AddRange(l);
+            //list.RemoveRange(5, 4);
+            //list.Remove(1);
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);    
+            //}
+            
+
+            Dictionary<string, string> capitals = new();
+            capitals.Add("Aze", "Baku");
+            capitals.Add("Eng", "London");
+            capitals.Add("Canada", "Ottawa");
+            Console.WriteLine(capitals["Aze"]);
+            foreach (var item in capitals)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ C#/Lesson10/Lesson10/Program.cs
@@ -0,0 +1,59 @@
+﻿//////string[,] strings = new string[2, 2]
+//////{
+//////    { "hakuna", "Matata" },
+//////    { "john", "doe" }
+//////};
+
+//////Console.WriteLine(strings[0,0]);
+
+////string[][] strings = new string[2][]
+////{
+////    new string[] { "hakuna", "matata" },
+////    new string[] { "john", "doe" },
+////};
+
+////foreach (var item in strings[0])
+////{
+////}
+
+//namespace Lesson10;
+
+//class Program
+//{
+//    interface IA { }
+//    class A : IA { }
+//    class B : A { }
+//    class C { }
+//    static void  TemplateMethod1<T>(T arg) where T : IA, new()
+//    {
+//        Console.WriteLine(typeof(T).FullName);
+//    }
+//    static TResult? TemplateMethod<TResult,T>(T arg)  where TResult : class, new()
+//    {
+//        Console.WriteLine(typeof(T).FullName);
+//        return null;
+//    }
+//    static void Main(string[] args)
+//    {
+//        //TemplateMethod1(new A());
+//        //TemplateMethod<double, Program>(new Program());
+//        //TemplateMethod<Program, double>(4.6);
+//        //Some<int> some = new Some<int>();
+//        //var some1 = new Some<int>();
+//        //Some<string> some2 = new();
+//        Pair<int, string> pair = new() { Key = 1, Value =  "Hakuna"};
+//        Console.WriteLine(pair.Key);
+//        Console.WriteLine(pair.Value);
+//    }
+
+//    class Pair<TKey, TValue>
+//    {
+//        public TKey Key { get; set; }
+//        public TValue Value { get; set; }
+//    }
+
+//    class Some<T> where T : struct
+//    {
+//        public T MyProperty { get; set; }
+//    }
+//}
