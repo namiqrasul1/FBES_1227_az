a488f1b7915e3bb5d7e92694318f6c7dbb4e4eb6
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2023-12-22 19:09:10 +0400

    Lesson4

---
+++ Asp.net/FirstMvc/FirstMvc/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using FirstMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FirstMvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly static List<Product> products = new()
+        {
+            new Product { Name = "Cola", Price = 2},
+            new Product { Name = "Fanta", Price = 2},
+            new Product { Name = "Sprite", Price = 2},
+        };
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            //Product? product = products.FirstOrDefault(p => p.Id == id);
+            //return View(product);
+
+            //ViewBag.Product = products.FirstOrDefault(p => p.Id == id);
+
+            //ViewData["Product"] = products.FirstOrDefault(p => p.Id == id);
+            TempData["Product"] = JsonSerializer.Serialize(products.FirstOrDefault(p => p.Id == id));
+
+            return RedirectToAction("Some");
+        }
+
+        public IActionResult Some()
+        {
+            Product? product =  JsonSerializer.Deserialize<Product>(TempData["Product"]!.ToString() ?? "");
+            return RedirectToAction("GetAll");
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return View(products);
+        }
+    }
+}
---
+++ Asp.net/FirstMvc/FirstMvc/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstMvc.Models
+{
+    public class Product
+    {
+        public static int StaticId { get; set; } = 0;
+        public int Id { get; set; } = StaticId++;
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+    }
+}
